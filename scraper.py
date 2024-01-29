bfb3e893e8daa65dfc701d66840173782057a3d1
zhineng <zhineng@gmail.com>
2024-01-30 00:22:00 +0800

    3.4批次爬取子網頁的資料

--- scraper.py
+++ scraper.py
@@ -13,5 +13,16 @@ title_list = []
 for title in titles:
     title_list.append(title.getText())
 
-print(title_list)
+urls = soup.find_all('a',{'class':'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+tag_list = []
+
+for url in urls:
+    sub_response = requests.get(url.get('href'))
+    sub_soup = BeautifulSoup(sub_response.text,'lxml')
+    tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e2o6ii40'})
+    for tag in tags:
+        tag_list.append(tag.getText())
+
+print(tag_list)
+
 
